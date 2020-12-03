33a24b9e00b01768f99c6c960eb5a80330364769
강창현 <aaakch0316@gmail.com>
2020-12-04 08:22:40 +0900

    vue cli [+] add data

--- 6.vue_cli/vue-start/src/components/MyComponent.vue
+++ 6.vue_cli/vue-start/src/components/MyComponent.vue
@@ -1,10 +1,21 @@
 <template>
-  <h1>나의 첫 컴포넌트</h1>
+    <div>
+        <h1>Hanpy</h1>
+        <h2>{{ message }}</h2>
+    </div>
 </template>
 
 <script>
 export default {
     name: 'MyComponent',  // 이부분이 디버깅 용이다.
+    data: function () {
+        return {
+            message: 'hi',
+            posts: [
+                { id: 1, title: 'hi', content: 'hello' }
+            ]
+        }
+    }
 }
 </script>
 
