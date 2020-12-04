d39e1a7819d80cd3535f136dd6dc9c4cfdc7b07f
강창현 <aaakch0316@gmail.com>
2020-12-04 14:03:26 +0900

    vue cli [+] prop-emit init

--- 6.vue_cli/vue-start/src/components/MyComponent.vue
+++ 6.vue_cli/vue-start/src/components/MyComponent.vue
@@ -2,18 +2,29 @@
     <div>
         <h1>Hanpy</h1>
         <h2>{{ message }}</h2>
+        <ul>
+            <button @click="getPosts">데이터 모우기</button>
+            <li v-for="post in posts" :key="post.id">
+                {{ post.title }}
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
     name: 'MyComponent',  // 이부분이 디버깅 용이다.
     data: function () {
         return {
             message: 'hi',
-            posts: [
-                { id: 1, title: 'hi', content: 'hello' }
-            ]
+            posts: []
+        }
+    },
+    methods: {
+        getPosts: function() {
+            axios.get('https://koreanjson.com/posts')
+                .then(res => this.posts = res.data)
         }
     }
 }
