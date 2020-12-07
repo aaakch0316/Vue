94ded6e061481b7e58abc2f1e6fd0b144856b33b
강창현 <aaakch0316@gmail.com>
2020-12-07 15:38:46 +0900

    vue [+] axios youtube

--- youtube-browser/src/App.vue
+++ youtube-browser/src/App.vue
@@ -1,15 +1,41 @@
 <template>
   <div class="container">
-    뭐가뜨나 확인해보자.
+    <SearchBar @input-change="onInputChange" />
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+import SearchBar from './components/SearchBar.vue'
+
+const API_KEY = '######'
+const API_URL = 'https://www.googleapis.com/youtube/v3/search'
 
 export default {
   name: 'App',
   components: {
-
+    SearchBar,
+  },
+  data() {
+    return {
+      inputValue: '',
+      videos: [],
+    }
+  },
+  methods: {
+    onInputChange(inputText) {
+      this.inputValue = inputText
+      axios.get(API_URL, {
+        params: {
+          key: API_KEY,
+          part: 'snippet',
+          type: 'video',
+          q: this.inputValue,
+        }
+      })
+        .then(res => this.videos = res.data.items)
+        .catch(err => console.error(err))
+    }
   }
 }
 </script>
---
+++ youtube-browser/src/components/SearchBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+      <!-- change는 포커스밖으로 나가거나 엔터치거나 하면 함수 실행 -->
+      <!-- input으로 하면 키보드 누를 때 마다 반응한다. -->
+      <!-- keypress.enter 하면 엔터 칠 때만 된다. -->
+      <input @keypress.enter="onInput"> 
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'SearchBar',
+    methods: {
+        onInput(event) {
+            // console.log(event.target.value)
+            this.$emit('input-change', event.target.value)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
